bf946009439f568a00b8005a1afd8a78c1678f97
Josh <trinquele@gmail.com>
2018-04-04 16:27:51 -1000

    Initial Commit

---
+++ login-register/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  login-register
+//
+//  Created by Joshua  Lee on 4/4/18.
+//  Copyright © 2018 Joshua  Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
